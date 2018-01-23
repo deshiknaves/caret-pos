65ddefe18b2c73b7ae3dfbd5c5763daa811f940e
Deshi Rahim <deshi@deshiknaves.com>
2018-01-24 00:55:59 +1100

    Indicators

--- src/main.dev.js
+++ src/main.dev.js
@@ -4,19 +4,39 @@ import {
 } from './main';
 import { getOffset } from './utils';
 
-// Just tests
+const offsetBox = document.querySelector('.offset-box');
+const indicators = document.querySelector('.indicators');
+const offsetIndicator = indicators.querySelector('.offset-indicator');
+const positionBox = document.querySelector('.position-box');
+const positionIndicator = document.querySelector('.position-indicator');
+
+const setIndicators = (off, pos) => {
+  offsetBox.style.width = off.left + 'px';
+  offsetBox.style.height = Math.ceil(off.top) + 'px';
+  indicators.style.left = off.left + 'px';
+  indicators.style.top = (Math.ceil(off.top) + off.height)  + 'px';
+  offsetIndicator.innerHTML = `Offset: left: ${off.left}, top: ${off.top} height: ${off.height}`;
+  positionBox.style.width = pos.left;
+  positionBox.style.height = pos.top;
+  positionIndicator.innerHTML = `Position: left: ${pos.left}, top: ${pos.top}`;
+};
+
 const input = document.getElementById('input');
 input.addEventListener('click', () => {
   const pos = position(input);
+  const off = offset(input);
   console.log(pos);
-  console.log(offset(input, pos));
+  console.log(off);
+  setIndicators(off, pos);
 });
 
 const editable = document.getElementById('editable');
 editable.addEventListener('click', () => {
   const pos = position(editable);
+  const off = offset(editable);
   console.log(pos);
-  console.log(offset(editable, pos));
+  console.log(off);
+  setIndicators(off, pos);
 });
 
 const frame = document.getElementById('iframe');
@@ -29,8 +49,7 @@ body.addEventListener('click', () => {
   const off = offset(body, { iframe: frame });
   const frameOffset = getOffset(frame);
   off.left += frameOffset.left;
-  off.top = frameOffset.top;
+  off.top += frameOffset.top;
   const pos = position(body, { iframe: frame });
-  console.log(pos);
-  console.log(off);
+  setIndicators(off, pos);
 });
