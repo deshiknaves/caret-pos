9a84dd41a4bc6cc9bf55e3b4d54cbb8f2c8ae50a
Aram Drevekenin <grimsniffer@gmail.com>
2018-08-28 10:23:23 +0200

    test(offset): customPos

--- src/caret.spec.js
+++ src/caret.spec.js
@@ -33,6 +33,14 @@ describe('caret-pos', () => {
 
       expect(pos.pos).toBe(15);
     });
+
+    it('should correctly get the caret offset with a custom position', () => {
+      position(editor, 3);
+      const off = offset(editor, {customPos: 2});
+      expect(off.height).toBe(19);
+      expect(off.left).toBe(22);
+      expect(off.top).toBe(12);
+    });
   });
 
   describe('Editable', () => {
@@ -167,6 +175,14 @@ describe('caret-offset', () => {
       position(editor, 0);
       offset(editor, {noShadowCaret: true});
       expect(document.createTextNode).not.toHaveBeenCalled();
+    });
+
+    it('should correctly get the caret offset with a custom position', () => {
+      position(editor, 3);
+      const off = offset(editor, {customPos: 2});
+      expect(off.height).toBe(19);
+      expect(off.left).toBe(31);
+      expect(off.top).toBe(8);
     });
   })
 })
