0c22fb7b47d0bdd9af51d7afa24bd75b5300d5ce
Deshi Rahim <deshi@deshiknaves.com>
2018-08-28 21:40:28 +1000

    tests(Caret): Fixed osx sizes

--- src/caret.spec.js
+++ src/caret.spec.js
@@ -37,9 +37,9 @@ describe('caret-pos', () => {
     it('should correctly get the caret offset with a custom position', () => {
       position(editor, 3);
       const off = offset(editor, { customPos: 2 });
-      expect(off.height).toBe(16);
-      expect(off.left).toBe(19);
-      expect(off.top).toBe(0);
+      expect(off.height).toBe(14);
+      expect(off.left).toBe(29);
+      expect(off.top).toBe(12);
     });
   });
 
@@ -166,9 +166,9 @@ describe('caret-offset', () => {
     it('should correctly get the caret offset', () => {
       position(editor, 3);
       const off = offset(editor);
-      expect(off.height).toBe(16);
-      expect(off.left).toBe(29);
-      expect(off.top).toBe(0);
+      expect(off.height).toBe(14);
+      expect(off.left).toBe(33);
+      expect(off.top).toBe(8);
     });
 
     it('should respect noShadowCaret', () => {
@@ -180,9 +180,9 @@ describe('caret-offset', () => {
     it('should correctly get the caret offset with a custom position', () => {
       position(editor, 3);
       const off = offset(editor, { customPos: 2 });
-      expect(off.height).toBe(16);
-      expect(off.left).toBe(19);
-      expect(off.top).toBe(0);
+      expect(off.height).toBe(14);
+      expect(off.left).toBe(25);
+      expect(off.top).toBe(8);
     });
   });
 });
